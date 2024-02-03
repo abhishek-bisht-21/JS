d566875b849fc4c0bc803c437495716a3dba47c6
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-02-03 17:06:41 +0530

    Unary + operator in Coercion

--- First Pillar of JS Coercion/6.+OperatorCoersion.js
+++ First Pillar of JS Coercion/6.+OperatorCoersion.js
@@ -1,1 +1,5 @@
 console.log(typeof +"100") // Unary + operator calls ToNumber
+
+let obj = {}
+console.log(120 + obj) // ToPrimitive on Both Operands -> toString method return [object Object]
+// Hence Output: 120[object Object]
