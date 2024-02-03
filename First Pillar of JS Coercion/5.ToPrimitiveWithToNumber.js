09d9502593f051a32b7a2d294c5c08bb5bd5f6ed
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-02-03 17:01:05 +0530

    toPrimitive With ToNumber

--- First Pillar of JS Coercion/5.ToPrimitiveWithToNumber.js
+++ First Pillar of JS Coercion/5.ToPrimitiveWithToNumber.js
@@ -2,3 +2,15 @@ let obj = {};
 
 // ToNumber called on both operand -> ToPrimitive(obj, hint: number) -> OrdinaryToPrimitive(obj, hint: number) -> ["valueOf", "string"]
 console.log(10 - obj); // NaN
+
+let obj1 = { x: 7, y: 8 };
+console.log(100 - obj1); // NaN
+
+let obj2 = { x: 7, valueOf() { return 99 } };
+console.log(100 - obj2); // 1
+
+let obj3 = { x: 9, valueOf() { return obj2 }, toString() { return "99" } };
+console.log(100 - obj3); // 1
+
+let obj4 = { x: 10, toString() { return {} } };
+console.log(100 - obj4); // TypeErr
