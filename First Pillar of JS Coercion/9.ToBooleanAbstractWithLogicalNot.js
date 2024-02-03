152cf95afae79f41228919a69d1f4cd95092e67a
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-02-03 17:19:50 +0530

    Equality Operator

---
+++ First Pillar of JS Coercion/9.ToBooleanAbstractWithLogicalNot.js
@@ -0,0 +1,7 @@
+let x = 10;
+console.log(!x); // ToBoolean(10) -> true -> !true = false
+
+let y = undefined;
+console.log(!y); // ToBoolean(undefined) -> false -> !false = true
+
+// if-else can be used to determine if the Value is Truthy or Falsy
