ce882f356b9e42186b9998cb94b84e7c54b8eb60
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-02-03 17:12:12 +0530

    Equality Operator

--- First Pillar of JS Coercion/6.+OperatorCoersion.js
+++ First Pillar of JS Coercion/6.+OperatorCoersion.js
@@ -3,3 +3,5 @@ console.log(typeof +"100") // Unary + operator calls ToNumber
 let obj = {}
 console.log(120 + obj) // ToPrimitive on Both Operands -> toString method return [object Object]
 // Hence Output: 120[object Object]
+
+console.log("120" + obj) //"120" "[object Object]" -> Both of them are string -> concatenation
---
+++ First Pillar of JS Coercion/7.EqualityOperators.js
@@ -0,0 +1,4 @@
+// == (abstract equality) and === (Strict Equality) both of them checks Type. 
+// But what they do after that is different
+
+
