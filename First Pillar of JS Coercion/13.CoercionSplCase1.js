2d2cc58cdecba63ed11048ef4cf3b0be29414c1c
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-02-04 12:21:13 +0530

    Coercion Special Cases

--- First Pillar of JS Coercion/13.CoercionSplCase1.js
+++ First Pillar of JS Coercion/13.CoercionSplCase1.js
@@ -10,3 +10,12 @@ console.log("" + [1,2,3]); // 1,2,3
 
 
 console.log("" + [null, undefined]); // ,
+
+
+// ToNumber
+console.log(0 - "010"); // get converted to decimal number
+console.log(0 - "O10");
+console.log(0 - 010); // gets converted to Octal Number
+console.log(0 - "0xb") // gets converted to hexadecimal
+console.log([] - 1); // Empty array gets converted to 0
+console.log([""] - 1); // Empty array with string gets converted to 0
