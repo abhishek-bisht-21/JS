f03047800cf9acfa856bea24c4056bec8433e86f
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-02-04 12:28:52 +0530

    Coercion with tempolated strings

---
+++ First Pillar of JS Coercion/14.CoercionWithTempolatedString.js
@@ -0,0 +1,8 @@
+console.log("My fav number is "+21);
+console.log("My fav number is ", 10);
+let x = 8
+console.log(`1my fav number is ${x}`); // x gets converted ToString
+
+
+let obj = {x:10, y:21};
+console.log(`my object is ${obj}`); // [object Object]
