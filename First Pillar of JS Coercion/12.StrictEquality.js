e3f973c1232d90a3c7a29a207569f6af26487df3
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-02-04 11:56:20 +0530

    Strict Equality comparison

--- First Pillar of JS Coercion/12.StrictEquality.js
+++ First Pillar of JS Coercion/12.StrictEquality.js
@@ -5,3 +5,15 @@ console.log("9" === 9); // false
 let obj = {x: 10, valueOf(){return 100}};
 console.log(99 == obj);// false
 console.log(100 == obj); // true ToPrimitive(obj, hint: number)
+
+
+
+
+let obj1 = {x:10};
+let obj2 = {x:10};
+let obj3 = {x:10};
+
+console.log(obj1 === obj2); // false (Even though the key-value pairs are same but they are present at diff memory location in memory they are not equal)
+console.log(obj1 === obj3); // false
+console.log(obj1 === obj1); // true
+console.log({x:10} === {x:10});
