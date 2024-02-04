8408288d5ee24f8d62e2c9a1f63146f6285a2ef8
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-02-04 14:16:52 +0530

    Assignment

---
+++ First Pillar of JS Coercion/16.Assignment.js
@@ -0,0 +1,27 @@
+function ToDoApp(){
+	this.tasks = [];
+}
+
+ToDoApp.prototype.addTask = function(task){
+	if(task != null && typeof task === 'object'){
+		this.tasks.push(task["text"]);
+	}else{
+		this.tasks.push(task);
+	}
+}
+
+ToDoApp.prototype.displayTasks = function(){
+	this.tasks.forEach(function(task, index){
+		console.log(index + ': ' + task);
+	});
+}
+
+
+// usage
+var myToDo = new ToDoApp();
+myToDo.addTask({text: "Buy groceries"});
+myToDo.addTask("Attend meetings at 10AM");
+myToDo.addTask(["Call John", "Pay Bills"]);
+myToDo.displayTasks();
+
+
