74a121d590f51b52e5a9398a66e2bcc080ad6f75
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-02-08 10:09:21 +0530

    function Expression

---
+++ 2.Second Pillar Of JS Scopes/13.functionExpression.js
@@ -0,0 +1,24 @@
+function fun(){   // function declaration -> starts with the keyword function
+	// some impl
+}
+
+let f = function gun(){ // function expression
+	// some impl
+}
+
+let a = function(){ // function expression
+	// okk some more impl
+}
+
+
+(function x(){  // function expression
+	// can you stop it
+})
+
+(function(){  // function expression
+	// i am done
+})
+
+let y = () => {   // function expression
+ 
+}
