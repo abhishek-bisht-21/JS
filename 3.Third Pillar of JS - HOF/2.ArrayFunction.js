f2f1623c0c027098af8213def711c6f78f08c5c7
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-02-10 12:36:02 +0530

    Map function in Arrays

--- 3.Third Pillar of JS - HOF/2.ArrayFunction.js
+++ 3.Third Pillar of JS - HOF/2.ArrayFunction.js
@@ -11,16 +11,29 @@
  * Map Function
  * Map is a higher order function available with arrays. It takes a function as an argument -> f
  * It returns an arrayin which every value is actually populated by calling a function f with original array element as argument.
+ * 
+ * Every element of the original array is one by one passed to teh argument function 'f'.
+ * Whatever is the output for each individual element, we populate that output in an array.
+ * 
+ * Map internally iterates/loop over every element of the original array pass that element in the arugment function f and 
+ * then stores the returned value inside an array.
  */
 
-function square(el){
+function square(el){ // return square
 	return el*el;
 }
 
-const arr = [1,2,3,4,5];
+function cube(el){
+	return el*el*el;
+}
 
-const res = arr.map(square);
+const arr = [1,2,3,4,5]; // original
+
+const res = arr.map(square); // sqaure is function passed as an argument
+const cubeArr = arr.map(cube);
 console.log("Array arr: ", arr);
 console.log("Array res: ", res); // [1,4,9,16,25]
+console.log("Array cubeArr: ", cubeArr); // [1,8,27,64,125]
+
 
 
