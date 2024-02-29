cee9b0fa850e313fb3b44b829f7d0edb4621eacf
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-02-29 23:52:08 +0530

    Downloading function Using Callbacks and Promise

--- 6.Sixth Pillar of JS- Promises/13.PromiseResolvesCallbacksIssues.js
+++ 6.Sixth Pillar of JS- Promises/13.PromiseResolvesCallbacksIssues.js
@@ -1,6 +1,7 @@
-function download(url, cb){
+// callback downloader dummy
+function download(url, cb) {
 	console.log("Started downloading from url", url);
-	setTimeout(function exec(){
+	setTimeout(function exec() {
 		console.log("Completed downloading after 5s");
 		const content = "ABCDEF";
 		cb(content); // What if don't call/invoke our callback function the manner which is required.
@@ -8,6 +9,38 @@ function download(url, cb){
 }
 
 
-download("www.xyz.com", function processDownload(data){
+download("www.xyz.com", function processDownload(data) {
 	console.log("download data is", data);
 })
+
+/*
+OUTPUT:
+Started downloading from url www.xyz.com
+Completed downloading after 5s
+download data is ABCDEF
+
+*/
+
+// Promise Download dummmy
+
+function download(url) {
+	return new Promise(function exec(res, rej) {
+		console.log("Promise: Started downloading from url", url);
+		setTimeout(function () {
+		console.log("Promise: Completed downloading after 5s");
+		res("ABCDEF");	
+		}, 5000);
+	})
+}
+
+let downloadCompleted = download("www.zyz.com");
+downloadCompleted.then(function(){
+	console.log("Fullfilled Handler called");
+})
+
+/**
+ * OUTPUT:
+Promise: Started downloading from url www.zyz.com
+Promise: Completed downloading after 5s
+Fullfilled Handler called
+ */
