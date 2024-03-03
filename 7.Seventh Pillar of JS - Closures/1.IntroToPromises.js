28ba3d10e350aaf5da709676e229d99334d756b5
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-03-03 13:41:49 +0530

    Intro to Closures

---
+++ 7.Seventh Pillar of JS - Closures/1.IntroToPromises.js
@@ -0,0 +1,23 @@
+function process(){
+	let i=0;
+	function innerProcess(){
+		i+=1;
+		return i;
+	}
+
+	return innerProcess;
+}
+
+let res = process(); //calls the function, which returns another function
+console.log(res);
+console.log("first time calling res", res()); //1
+console.log("second time calling res", res()); //2
+console.log("third time calling res", res()); //3
+
+
+/**
+ * When parsing goes on we know that 'i' is coming from the scope of scope of cuntion process.
+ * Due to Closure property 'i' is accesible to innerProcess function even though prcoess function is
+ * no longer in the CallStack. Because of Closure it knows the exact memory Location of any variable
+ * who is used in the inner functions. The Same variable 'i' is getting updated.
+ */
