3ea1b12d39022b4480a46f2e1100c354a7819e87
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2024-03-05 09:15:57 +0530

    Generator demo 2

---
+++ 7.Seventh Pillar of JS - Closures/6.GeneratorDemo2.js
@@ -0,0 +1,16 @@
+function* fetchNextElement(){
+	console.log("Inside generator")
+	const x =10;
+	yield 11;
+	console.log("Entering after a yield");
+	const y = x + (yield 30);
+	console.log("Value of y is", y);
+}
+
+console.log("start")
+const iter = fetchNextElement();
+console.log("called Generator")
+console.log("first", iter.next());
+console.log("second", iter.next());
+console.log("third", iter.next());
+
